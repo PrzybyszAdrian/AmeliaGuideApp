59cf2431de3b1feef693c4a78b2e943d2d4962bc
Adrian Przybysz <35032840+PrzybyszAdrian@users.noreply.github.com>
2024-10-09 20:09:50 +0200

    Update login-btn.tsx

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -6,13 +6,14 @@ export default function LogInButton() {
   if (session) {
     return (
       <div className="max-w-48 space-y-3">
-
+        <Button onClick={() => signOut()}>Sign out</Button>
+        <Button className="overflow-hidden text-ellipsis ml-1 bg-[#183534] hover:bg-[#183534] p-3 rounded-lg shadow-sm ">{session.user?.name}</Button>
       </div>
     );
   }
   return (
     <>
-   
+      <Button onClick={() => signIn()}>Sign in</Button>
     </>
   );
 }
